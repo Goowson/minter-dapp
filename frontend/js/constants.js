2ee77d3de75f828e988f35f204ab6738f3a1185a
Goowson <b.guihot@hotmail.com>
2022-05-13 21:53:34 +1000

    Test Net Deploy

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xE7D69ABBbDD9eE3F90dE92CCC17454FE88C361D2"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
